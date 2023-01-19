6af74a20cacaeef1e127578ca09c691593b0bed2
Bobby de Vos <bobby_devos@sil.org>
2023-01-19 06:20:53 -0700

    Improve GDEF mark class

--- tools/archive/gdef.py
+++ tools/archive/gdef.py
@@ -8,12 +8,15 @@ ufo = sys.argv[1]
 font = OpenFont(ufo)
 
 # Modify UFO
-cgj = 0x034F
-vs = range(0xFE00, 0xFE0F+1)
-mn = [cgj] + list(vs)
+
+latn = (0x0060, 0x00A8, 0x00AF, 0x00B4, 0x00B8, 0x02C6, 0x02C7, 0x02D8, 0x02DB, 0x02D9, 0x02DA, 0x02DC, 0x02DD)
 
 for glyph in font:
-    if glyph.unicode in mn:
+    if glyph.unicode in latn:
+        for anchor in glyph.anchors:
+            if anchor.name.startswith('_'):
+                glyph.removeAnchor(anchor)
+    if glyph.unicode == 0x11D97: # virama
         glyph.appendAnchor('_none', (0, 0))
 
 # Save UFO
